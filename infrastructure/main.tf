a94035224e50b8803b70f1d9cbb636ff8d89a5ae
Ryan Wallace <rwallaceguitar@gmail.com>
2025-02-05 15:38:33 -0800

    readme

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,15 +6,19 @@ terraform {
     }
   }
 }
+
+#define aws as provider and region to use
 provider "aws" {
   region = "us-east-1"
 }
 
+#set security rules for instance
 resource "aws_security_group" "allow_ssh_http" {
   name = "allow_ssh_http"
   description = "Allow SSH and HTTP access"
 
   ingress {
+    #allow ssh
     from_port = 22
     to_port = 22
     protocol = "tcp"
@@ -22,6 +26,7 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
+    #allow http
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
@@ -29,6 +34,7 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   egress {
+    #allow responses
     from_port = 0
     to_port = 0
     protocol = "-1"
@@ -36,16 +42,18 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
+#define ec2 instance
 resource "aws_instance" "terraform_server" {
-  ami = "ami-0c614dee691cbbf37"
-  instance_type = "t2.micro"
-  key_name = "DemoKeyPair"
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami = "ami-0c614dee691cbbf37" #default amazon linux AMI
+  instance_type = "t2.micro" #instance size
+  key_name = "DemoKeyPair" # previously existing key pair
+  security_groups = [aws_security_group.allow_ssh_http.name] #references earlier defined security rules
 
   tags = {
     name = "Demo-EC2"
   }
 
+  #verifies ssh into new ec2 instance
   provisioner "remote-exec" {
     inline = ["echo 'EC2 instance created'"]
 
@@ -57,11 +65,13 @@ resource "aws_instance" "terraform_server" {
     }
   }
 
+  #after verifying ssh, runs ansible playbook to set up docker image
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' --private-key /mnt/c/Users/rwall/Downloads/DemoKeyPair.pem playbook.yml"
   }
 }
 
+#output ip address for us to reference as needed
 output "ec2_public_ip" {
   value = aws_instance.terraform_server.public_ip
 }
