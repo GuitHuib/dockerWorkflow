4d44dc1a0362c3591af26941577675a5e11e0309
Ryan Wallace <rwallaceguitar@gmail.com>
2025-02-06 10:30:04 -0800

    remove outdated line

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "terraform_server" {
   ami = "ami-0c614dee691cbbf37" #default amazon linux AMI
   instance_type = "t2.micro" #instance size
   key_name = "DemoKeyPair" # previously existing key pair
-  security_groups = [aws_security_group.allow_ssh_http.name] #references earlier defined security rules
+  # security_groups = [aws_security_group.allow_ssh_http.name] #references earlier defined security rules
 
   tags = {
     name = "Demo-EC2"
