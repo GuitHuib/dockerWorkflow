12030970ef956cbf8c29acf5990499c6feb739a3
Ryan Wallace <rwallaceguitar@gmail.com>
2025-02-06 16:15:29 -0800

    terraform plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,6 +41,15 @@ resource "aws_security_group_rule" "allow_http" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = data.aws_security_group.existing_sg.id
 }
+
+resource "aws_security_group_rule" "allow_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.existing_sg.id
+}
 # resource "aws_security_group" "allow_ssh_http" {
 #   name = "demo_app_security"
 #   description = "Allow SSH and HTTP access"
